f98a2312b7b4430757b5a696e00f47c47dda8a2b
thcl-odoo <thcl@odoo.com>
2024-02-15 16:38:56 +0100

    [REF] remove unnecessary stuff and fix little issue
    - Unnecessary background script and event to handle the openning of Odoo.SH Projects page.
    The `"matches": ["https://*.odoo.sh/project*"]` is here for that. Each time you reach a webpage that matches this pattern, the related JS file (here `contentScript.js` is called)
    
    - Don't forget to add `const` or `let` for the variable.. we aren't on python :cry:

--- contentScript.js
+++ contentScript.js
@@ -1,60 +1,50 @@
-(() => {
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value } = obj;
+const shLoaded = () => {
+    const starExists = document.getElementsByClassName("star")[0];
 
-        if (type === "NEW"){
-            shLoaded();
-        }
-    });
+    if (!starExists) {
+        // Set star image
+        const star = document.createElement("img");
+        star.style.width = "25px";
+        star.style.height = "25px";
+        star.src = chrome.runtime.getURL("assets/star.png");
+        star.className = "ytp-button " + "star";
+        star.title = "Click to star project";
 
-    const shLoaded = () => {
-        const starExists = document.getElementsByClassName("star")[0];
+        // Add star to each cards
+        const cardContainers = document.getElementsByClassName("o_project_card_container");
+        for (let i = 0; i < cardContainers.length; i++) {
+            const card_container = cardContainers[i];
+            const firstDivA = card_container.querySelector("div");
+            const firstDiv = firstDivA.querySelector("div");
+            if (firstDiv) {
+                const clonedStar = star.cloneNode(true);
+                firstDiv.appendChild(clonedStar); // Append the star image inside the first div
 
-        if (!starExists) {
-            // Set star image
-            const star = document.createElement("img");
-            star.style.width = "25px";
-            star.style.height = "25px";
-            star.src = chrome.runtime.getURL("assets/star.png");
-            star.className = "ytp-button " + "star";
-            star.title = "Click to star project";
-
-            // Add star to each cards
-            card_containers = document.getElementsByClassName("o_project_card_container");
-            for (let i = 0; i < card_containers.length; i++) {
-                const card_container = card_containers[i];
-                const firstDivA = card_container.querySelector("div");
-                const firstDiv = firstDivA.querySelector("div");
-                if (firstDiv) {
-                    const clonedStar = star.cloneNode(true);
-                    firstDiv.appendChild(clonedStar); // Append the star image inside the first div
-
-                    clonedStar.addEventListener("mouseover", () => {
-                        clonedStar.style.opacity = "0.7";
-                        //clonedStar.style.backgroundColor = "lightgray";
-                    });
-                    clonedStar.addEventListener("mouseout", () => {
-                        clonedStar.style.opacity = "1";
-                        //clonedStar.style.backgroundColor = "";
-                    });
-                    clonedStar.addEventListener("click", addNewStarEventHandler(card_container));
-                }
+                clonedStar.addEventListener("mouseover", () => {
+                    clonedStar.style.opacity = "0.7";
+                    //clonedStar.style.backgroundColor = "lightgray";
+                });
+                clonedStar.addEventListener("mouseout", () => {
+                    clonedStar.style.opacity = "1";
+                    //clonedStar.style.backgroundColor = "";
+                });
+                clonedStar.addEventListener("click", () => addNewStarEventHandler(card_container));
             }
         }
     }
+}
 
-    const addNewStarEventHandler = (card) => {
-        const project = card;
+const addNewStarEventHandler = (card) => {
+    const project = card;
 
-         // Storing the project name in Chrome storage
-        chrome.storage.sync.get(["starredProjects"], function(result) {
-            let starredProjects = result.starredProjects || [];
-            starredProjects.push(project);
-            chrome.storage.sync.set({ "starredProjects": starredProjects }, function() {
-                console.log('Project stared');
-            });
+    // Storing the project name in Chrome storage
+    chrome.storage.sync.get(["starredProjects"], function(result) {
+        const starredProjects = result.starredProjects || [];
+        starredProjects.push(project);
+        chrome.storage.sync.set({ "starredProjects": starredProjects }, function() {
+            console.log('Project stared', project);
         });
-    }
+    });
+}
 
-    shLoaded();
-})();
+shLoaded();
